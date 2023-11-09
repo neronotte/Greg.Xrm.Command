56b3c514f57ad34cce9a3daf4536bb82c3738951
_neronotte <riccardo.gregori@outlook.com>
2023-11-09 21:39:55 +0100

    Added export field metadata command

--- Greg.Xrm.Command.Core/Commands/Solution/DeleteCommand.cs
+++ Greg.Xrm.Command.Core/Commands/Solution/DeleteCommand.cs
@@ -1,6 +1,6 @@
 ﻿namespace Greg.Xrm.Command.Commands.Solution
 {
-	[Command("solution", "delete", HelpText = "Deletes a solution from the current Dataverse environment.")]
+	[Command("solution", "delete", HelpText = "Deletes a solution from the current Dataverse environment")]
 	public class DeleteCommand
 	{
 		[Option("uniqueName", "un", IsRequired = true, HelpText = "The unique name of the solution to delete.")]
