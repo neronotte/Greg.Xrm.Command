732f6fe9c1c8403e99b27620f87c2bb12d8330a7
Simone Giubbarelli <simone.giubbarelli@avanade.com>
2023-11-30 22:24:27 +0100

    Delete Column

---
+++ Greg.Xrm.Command.Core/Commands/Column/DeleteCommandExecutor.cs
@@ -0,0 +1,57 @@
+﻿using Greg.Xrm.Command.Services.Connection;
+using Greg.Xrm.Command.Services.Output;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using System.ServiceModel;
+
+namespace Greg.Xrm.Command.Commands.Column
+{
+    public class DeleteCommandExecutor : ICommandExecutor<DeleteCommand>
+    {
+        private readonly IOutput output;
+        private readonly IOrganizationServiceRepository organizationServiceRepository;
+
+        public DeleteCommandExecutor(
+            IOutput output,
+            IOrganizationServiceRepository organizationServiceRepository)
+        {
+            this.output = output;
+            this.organizationServiceRepository = organizationServiceRepository;
+        }
+
+
+        public async Task ExecuteAsync(DeleteCommand command, CancellationToken cancellationToken)
+        {
+            this.output.Write($"Connecting to the current dataverse environment...");
+            var crm = await this.organizationServiceRepository.GetCurrentConnectionAsync();
+            this.output.WriteLine("Done", ConsoleColor.Green);
+
+
+            try
+            {
+                output.Write($"Deleting column ").Write(command.SchemaName, ConsoleColor.Yellow).Write(" from table ").Write(command.EntityName, ConsoleColor.Yellow).Write("...");
+
+                var request = new DeleteAttributeRequest
+                {
+                    EntityLogicalName = command.EntityName,
+                    LogicalName = command.SchemaName
+                };
+
+                await crm.ExecuteAsync(request);
+
+                output.WriteLine(" Done", ConsoleColor.Green);
+            }
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                output.WriteLine()
+                    .Write("Error: ", ConsoleColor.Red)
+                    .WriteLine(ex.Message, ConsoleColor.Red);
+
+                if (ex.InnerException != null)
+                {
+                    output.Write("  ").WriteLine(ex.InnerException.Message, ConsoleColor.Red);
+                }
+            }
+        }
+    }
+}
