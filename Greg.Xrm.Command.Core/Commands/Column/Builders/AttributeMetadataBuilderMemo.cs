c21dfde544740d5124028038d507c0f712a01a17
Simone Giubbarelli <simone.giubbarelli@avanade.com>
2023-12-14 10:46:08 +0100

    Enum / non enum on MemoFormatName

--- Greg.Xrm.Command.Core/Commands/Column/Builders/AttributeMetadataBuilderMemo.cs
+++ Greg.Xrm.Command.Core/Commands/Column/Builders/AttributeMetadataBuilderMemo.cs
@@ -13,7 +13,7 @@ namespace Greg.Xrm.Command.Commands.Column.Builders
             // Set extended properties
             //overridden of default values of commands in common with Text command
             attribute.Format = command.StringFormat == StringFormat.Text ? StringFormat.TextArea : command.StringFormat;
-            attribute.FormatName = command.MemoFormat;
+            attribute.FormatName = GetFormatName(command.MemoFormat);
             attribute.ImeMode = command.ImeMode == ImeMode.Disabled ? ImeMode.Auto : command.ImeMode;
             attribute.MaxLength = GetMaxLength(command.MaxLength);
 
@@ -21,6 +21,18 @@ namespace Greg.Xrm.Command.Commands.Column.Builders
         }
 
 
+        private static MemoFormatName GetFormatName(MemoFormatName1 memoFormatName1)
+        {
+            return memoFormatName1 switch
+            {
+                MemoFormatName1.Text => MemoFormatName.Text,
+                MemoFormatName1.Email => MemoFormatName.Email,
+                MemoFormatName1.Json => MemoFormatName.Json,
+                MemoFormatName1.RichText => MemoFormatName.RichText,
+                MemoFormatName1.TextArea => MemoFormatName.TextArea,
+                _ => throw new NotSupportedException($"MemoFormatName1 {memoFormatName1} is not supported."),
+            };
+        }
         internal int GetMaxLength(int? maxLength)
         {
             if (maxLength == null) return 2000;
