342e08e24a741129934e6001bdbf9b67c755218f
_neronotte <riccardo.gregori@outlook.com>
2024-09-30 10:42:45 +0200

    updated to .net 8

--- Greg.Xrm.Command.Core/Commands/WebResources/JsCreateCommand.cs
+++ Greg.Xrm.Command.Core/Commands/WebResources/JsCreateCommand.cs
@@ -4,7 +4,7 @@ namespace Greg.Xrm.Command.Commands.WebResources
 	[Command("webresources", "js", "create", HelpText = "(Preview) Creates a new Javascript webresource from a template")]
 	[Alias("wr", "js", "create")]
 	[Alias("wr", "create", "js")]
-	[Alias("wr", "create", "js")]
+	[Alias("webresources", "create", "js")]
 	public class JsCreateCommand
 	{
 		[Option("for", "f", HelpText = "Indicates if the JS web resource to create is for a form, a ribbon command, or other", DefaultValue = JavascriptWebResourceType.Form)]
