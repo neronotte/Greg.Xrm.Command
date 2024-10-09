39a59942e0cb0bd0a9eb77939c37bb2dc18550d7
_neronotte <riccardo.gregori@outlook.com>
2024-10-09 13:05:08 +0200

    aligned package version on .interfaces

---
+++ sample/SamplePlugin/SampleCommand.cs
@@ -0,0 +1,11 @@
+﻿//using Greg.Xrm.Command;
+
+namespace SamplePlugin
+{
+	//[Command("sample", "run", HelpText = "Sample plugin that does a simple echo")]
+	//public class SampleCommand
+	//{
+	//	[Option("echo", HelpText = "Echo message")]
+	//	public string? Echo { get; set; }
+	//}
+}
---
+++ sample/SamplePlugin/SampleCommandExecutor.cs
@@ -0,0 +1,21 @@
+﻿//using Greg.Xrm.Command;
+//using Greg.Xrm.Command.Services.Output;
+
+namespace SamplePlugin
+{
+	//public class SampleCommandExecutor : ICommandExecutor<SampleCommand>
+	//{
+	//	private readonly IOutput output;
+
+	//	public SampleCommandExecutor(IOutput output)
+ //       {
+	//		this.output = output;
+	//	}
+
+ //       public Task<CommandResult> ExecuteAsync(SampleCommand command, CancellationToken cancellationToken)
+	//	{
+	//		this.output.WriteLine($"Echo: {command.Echo ?? "-"}");
+	//		return Task.FromResult(CommandResult.Success());
+	//	}
+	//}
+}
