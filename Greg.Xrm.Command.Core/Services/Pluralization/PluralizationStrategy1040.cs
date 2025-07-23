5b517b19c5ac65c934db1726476febdccc250aea
_neronotte <riccardo.gregori@outlook.com>
2025-07-23 15:38:42 +0200

    Added basic italian pluralization rules

--- Greg.Xrm.Command.Core/Services/Pluralization/PluralizationStrategy1040.cs
+++ Greg.Xrm.Command.Core/Services/Pluralization/PluralizationStrategy1040.cs
@@ -4,7 +4,15 @@
     {
         public Task<string> GetPluralForAsync(string word)
         {
-            throw new NotImplementedException();
-        }
+            if (string.IsNullOrWhiteSpace(word)) return Task.FromResult(word);
+
+			if (word.EndsWith('a')) return Task.FromResult(string.Concat(word.AsSpan(0, word.Length - 1), "e"));
+			if (word.EndsWith('e')) return Task.FromResult(string.Concat(word.AsSpan(0, word.Length - 1), "i"));
+			if (word.EndsWith('i')) return Task.FromResult(word);
+			if (word.EndsWith('o')) return Task.FromResult(string.Concat(word.AsSpan(0, word.Length - 1), "i"));
+			if (word.EndsWith('e')) return Task.FromResult(string.Concat(word.AsSpan(0, word.Length - 1), "i"));
+
+			return Task.FromResult(word);
+		}
     }
 }
