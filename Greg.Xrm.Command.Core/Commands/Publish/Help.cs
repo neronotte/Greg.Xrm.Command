000ff9ea8765f3612b2fd6b3cb9b9e5a4b8d1b4f
_neronotte <riccardo.gregori@outlook.com>
2025-12-26 17:23:06 +0100

    Added publish all command

---
+++ Greg.Xrm.Command.Core/Commands/Publish/Help.cs
@@ -0,0 +1,11 @@
+﻿using Greg.Xrm.Command.Parsing;
+
+namespace Greg.Xrm.Command.Commands.Publish
+{
+	public class Help : NamespaceHelperBase
+	{
+		public Help() : base("Commands to manage manual customization publishing", "publish")
+		{
+		}
+	}
+}
---
+++ Greg.Xrm.Command.Core/Commands/Publish/PublishAllCommandExecutor.cs
@@ -0,0 +1,37 @@
+﻿using Greg.Xrm.Command.Services.Connection;
+using Greg.Xrm.Command.Services.Output;
+using Microsoft.Crm.Sdk.Messages;
+
+namespace Greg.Xrm.Command.Commands.Publish
+{
+	public class PublishAllCommandExecutor(
+		IOutput output,
+		IOrganizationServiceRepository organizationServiceRepository
+
+
+	) : ICommandExecutor<PublishAllCommand>
+	{
+		public async Task<CommandResult> ExecuteAsync(PublishAllCommand command, CancellationToken cancellationToken)
+		{
+			output.Write($"Connecting to the current dataverse environment...");
+			var crm = await organizationServiceRepository.GetCurrentConnectionAsync();
+			output.WriteLine("Done", ConsoleColor.Green);
+
+			output.Write($"Publishing all customizations...");
+			try
+			{
+				var request = new PublishAllXmlRequest();
+
+				await crm.ExecuteAsync(request);
+				output.WriteLine("Done", ConsoleColor.Green);
+			}
+			catch (Exception ex)
+			{
+				output.WriteLine("Failed", ConsoleColor.Red);
+				return CommandResult.Fail($"Error publishing customizations: {ex.Message}", ex);
+			}
+
+			return CommandResult.Success();
+		}
+	}
+}
